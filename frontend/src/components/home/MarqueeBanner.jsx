869cd185779978546cab9bd6799a280a723ac8fb
satyam6767 <satyamkumar6767@gmail.com>
2026-08-10 17:49:31 +0530

    LLC

--- frontend/src/components/home/MarqueeBanner.jsx
+++ frontend/src/components/home/MarqueeBanner.jsx
@@ -1,4 +1,5 @@
 import { FaStarOfLife } from "react-icons/fa";
+import "./MarqueeBanner.css";
 
 const items = [
   "NEW ARRIVALS",
@@ -7,33 +8,52 @@ const items = [
   "CO-ORD SETS",
   "KURTIS",
   "JEANS",
-  "BLAZERS",
-  "HANDBAGS",
+  "SALWAR SUITS",
 ];
+
+const WaveDecoration = ({ position }) => {
+  return (
+    <div className={`marquee-wave marquee-wave--${position}`}>
+      <svg
+        viewBox="0 0 1440 70"
+        preserveAspectRatio="none"
+        aria-hidden="true"
+      >
+        <path d="M0 32 C120 4 180 4 300 32 S480 60 600 32 S780 4 900 32 S1080 60 1200 32 S1320 4 1440 32" />
+
+        <path d="M0 37 C120 9 180 9 300 37 S480 65 600 37 S780 9 900 37 S1080 65 1200 37 S1320 9 1440 37" />
+
+        <path d="M0 42 C120 14 180 14 300 42 S480 70 600 42 S780 14 900 42 S1080 70 1200 42 S1320 14 1440 42" />
+
+        <path d="M0 47 C120 19 180 19 300 47 S480 75 600 47 S780 19 900 47 S1080 75 1200 47 S1320 19 1440 47" />
+      </svg>
+    </div>
+  );
+};
 
 const MarqueeBanner = () => {
   return (
-    <section className="bg-[#171A2C] overflow-hidden py-6">
+    <section className="marquee-banner">
 
-      <div className="flex whitespace-nowrap animate-marquee">
+      {/* Top decorative wave */}
+      <WaveDecoration position="top" />
 
+      {/* Marquee content */}
+      <div className="marquee-banner__track">
         {[...items, ...items].map((item, index) => (
           <div
             key={index}
-            className="flex items-center mx-5 shrink-0"
+            className="marquee-banner__item"
           >
-            <h2 className="text-white text-4xl font-extrabold uppercase tracking-wide">
-              {item}
-            </h2>
+            <h2>{item}</h2>
 
-            <FaStarOfLife
-              className="mx-12 text-[#B40D38]"
-              size={24}
-            />
+            <FaStarOfLife className="marquee-banner__star" />
           </div>
         ))}
-
       </div>
+
+      {/* Bottom decorative wave */}
+      <WaveDecoration position="bottom" />
 
     </section>
   );

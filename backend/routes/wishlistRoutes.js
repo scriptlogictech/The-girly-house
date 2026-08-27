56e6f379fad6f9132c1ed8384cb5797f1adb941c
satyam6767 <satyamkumar6767@gmail.com>
2026-08-27 13:26:20 +0530

    LLC

--- backend/routes/wishlistRoutes.js
+++ backend/routes/wishlistRoutes.js
@@ -1,16 +1,38 @@
 const express = require("express");
+
 const router = express.Router();
 
 const wishlistController = require("../controllers/wishlistController");
 const protect = require("../middleware/authMiddleware");
 
+// ==============================
 // Add Product to Wishlist
-router.post("/", protect, wishlistController.addToWishlist);
+// ==============================
 
+router.post(
+  "/add",
+  protect,
+  wishlistController.addToWishlist
+);
+
+// ==============================
 // Get Wishlist
-router.get("/", protect, wishlistController.getWishlist);
+// ==============================
 
+router.get(
+  "/",
+  protect,
+  wishlistController.getWishlist
+);
+
+// ==============================
 // Remove Product from Wishlist
-router.delete("/:productId", protect, wishlistController.removeFromWishlist);
+// ==============================
+
+router.delete(
+  "/:productId",
+  protect,
+  wishlistController.removeFromWishlist
+);
 
 module.exports = router;
